fab7b7264c345ec8bb000bfd17826c51469b78a5
Chen Ching Yen <92667133+ChenChingYen@users.noreply.github.com>
2023-03-18 21:54:10 +0800

    something

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import 'rsuite/dist/rsuite.min.css'
+import { Form, ButtonToolbar, Button, Input } from 'rsuite';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <div> */}
+      <Form>
+        <Form.Group controlId="name">
+          <Form.ControlLabel>Username</Form.ControlLabel>
+          <Form.Control name="name" />
+          <Form.HelpText>Username is required</Form.HelpText>
+        </Form.Group>
+        <Form.Group controlId="email">
+          <Form.ControlLabel>Email</Form.ControlLabel>
+          <Form.Control name="email" type="email" />
+          <Form.HelpText tooltip>Email is required</Form.HelpText>
+        </Form.Group>
+        <Form.Group controlId="password">
+          <Form.ControlLabel>Password</Form.ControlLabel>
+          <Form.Control name="password" type="password" autoComplete="off" />
+        </Form.Group>
+        <Form.Group>
+          <ButtonToolbar>
+            <Button appearance="primary">Login</Button>
+            <Button appearance="default">Sign up</Button>
+          </ButtonToolbar>
+        </Form.Group>
+      </Form>
+      {/* </div> */}
     </div>
   );
 }
